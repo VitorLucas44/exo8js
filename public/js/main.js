a1d543d5ed51239cd9ad7340e4552709deaf0123
VitorLucas44 <vitoriosolucas@hotmail.com>
2022-12-20 15:21:13 +0100

    mise a jour

--- public/js/main.js
+++ public/js/main.js
@@ -5,24 +5,50 @@
 // switch (jour) {
 //     case "lundi":
 //       console.log("On est lundi Tu as cours  à MolenGeek et tu as sport en soirée.");
+//       break;
+//       case "mardi":
+//       console.log("On est mardi Tu as cours  à MolenGeek et tu es libre ce soir.");
+//       break;
+//       case "mercredi":
+//       console.log("On est mercredi Tu as cours  à MolenGeek et tu vas faire les courses ce soir.");
+//       break;
+//       case "jeudi":
+//       console.log("On est jeudi Tu as cours  à MolenGeek et tu vas chez ta mère ce soir.");
+//       break;
+//       case "vendredi":
+//       console.log("On est vendredi Tu as cours  à MolenGeek et tu es libre ce soir.");
+//       break;
+//       case "samedi":
+//       console.log("On est le week-end tu es libre toute la journée");
+//       break;
+//       case "dimanche":
+//       console.log("On est le week-end tu es libre toute la journée");
+//       break;
 //       default:
-//       console.log("on est pas ce jour là");
+//       console.log("ce n'est pas un jour de la semaine");
 //  }
 
 
 
 // exo2
 
-// let meteo = prompt("il fait beau ou il pleut aujourd'hui ?")
+// let meteo = prompt("Quel temps fait il aujourd'hui ?")
 
 // switch (meteo) {
 //     case "il pleut":
 //       console.log("Il pleut je prends un parapluie ");
 //       break;
-//       case " il fait beau":
+//       case "il fait beau":
 //       console.log("Il y a un jolie soleil aujourd'hui");
+//       break;
+//       case "il fait nuageux":
+//       console.log("Il fait nuageux le soleil n'est pas voyant");
+//       break;
+//       case "il neige":
+//       console.log("Il neige, il fait froid prends une grosse veste et de bonne chaussure");
+//       break;
 //       default:
-//       console.log("on est pas ce jour là");
+//       console.log("Ce n'est pas un temps");
 //  }
 
 
@@ -47,26 +73,33 @@
 
 // exo4
 
-let nbr1 = prompt("Entrez le premier nombre");
-let operateur = prompt("Entrez l'opérateur (+, -, * ou /)");
-let nbr2 = prompt("Entrez le deuxième nombre");
-nbr1 = parseFloat(nbr1);
-nbr2 = parseFloat(nbr2);
-let resultat;
-switch (operateur) {
-  case "+":
-    resultat = nbr1 + nbr2;
-    break;
-  case "-":
-    resultat = nbr1 - nbr2;
-    break;
-  case "":
-    resultat = nbr1 - nbr2;
-    break;
-  case "/":
-    resultat = nbr1 / nbr2;
-    break;
-  default:
-    console.log("Opération non valide");
-}
-console.log(resultat);
+// let nbr1 = prompt("Entrez le premier nombre");
+// let operateur = prompt("Entrez l'opérateur (+, -, * ou /)");
+// let nbr2 = prompt("Entrez le deuxième nombre");
+// nbr1 = parseFloat(nbr1);
+// nbr2 = parseFloat(nbr2);
+// let resultat;
+// switch (operateur) {
+//   case "+":
+//     resultat = nbr1 + nbr2;
+//     break;
+//   case "-":
+//     resultat = nbr1 - nbr2;
+//     break;
+//   case "":
+//     resultat = nbr1 - nbr2;
+//     break;
+//   case "/":
+//     resultat = nbr1 / nbr2;
+//     break;
+//   default:
+//     console.log("Opération non valide");
+// }
+// console.log(resultat);
+
+
+
+
+// Bonus:
+
+
